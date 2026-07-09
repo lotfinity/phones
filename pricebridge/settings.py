107a8d50a7346f7f9a4c1fbe6e6251aac8d9ab77
lotfinity <medtouradmin@bagdadinvest.com>
2026-07-09 14:28:57 +0300

    Fix settings middleware string

--- pricebridge/settings.py
+++ pricebridge/settings.py
@@ -60,7 +60,7 @@ if DEBUG and DEBUG_TOOLBAR_AVAILABLE:
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware,
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
