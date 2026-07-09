0f7667d0095b6639a6fe7586fac7ae4ca3d60127
lotfinity <medtouradmin@bagdadinvest.com>
2026-07-09 17:44:59 +0300

    Load clean market models during app import

--- market/apps.py
+++ market/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class MarketConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'market'
+
+    def import_models(self):
+        super().import_models()
+        # Register clean v2 models that live outside the large legacy models.py file.
+        from . import clean_models  # noqa: F401
